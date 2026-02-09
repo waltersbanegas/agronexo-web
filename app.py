832e624c739f22d39c2b115eb3acf0ae95d4c072
waltersbanegas <waltersbanegas@gmail.com>
2026-02-08 21:37:52 -0300

    V15 - Sistema Total con Mapa

--- app.py
+++ app.py
@@ -8,8 +8,7 @@ app = Flask(__name__)
 CORS(app)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-# Base de datos estable v14
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'agronexo_v14_final.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'agronexo_v15_pro.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -25,6 +24,7 @@ class Lote(db.Model):
     nombre = db.Column(db.String(100))
     cultivo = db.Column(db.String(50))
     has = db.Column(db.Float)
+    geometria = db.Column(db.Text) # Almacena el área dibujada
 
 class Lluvia(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -54,7 +54,7 @@ def gestion_datos(modulo):
     if request.method == 'POST':
         d = request.json
         if modulo == 'ganaderia': db.session.add(Animal(caravana=d['caravana'], peso=d['peso'], estado=d['estado']))
-        elif modulo == 'lotes': db.session.add(Lote(nombre=d['nombre'], cultivo=d['cultivo'], has=d['has']))
+        elif modulo == 'lotes': db.session.add(Lote(nombre=d['nombre'], cultivo=d['cultivo'], has=d['has'], geometria=d.get('geometria')))
         elif modulo == 'lluvias': db.session.add(Lluvia(mm=d['mm'], fecha=d['fecha']))
         elif modulo == 'gastos': db.session.add(Gasto(concepto=d['concepto'], monto=d['monto'], fecha=d['fecha']))
         db.session.commit()
@@ -62,7 +62,7 @@ def gestion_datos(modulo):
     
     items = model.query.all()
     if modulo == 'ganaderia': return jsonify([{"id":i.id,"caravana":i.caravana,"peso":i.peso,"estado":i.estado} for i in items])
-    if modulo == 'lotes': return jsonify([{"id":i.id,"nombre":i.nombre,"cultivo":i.cultivo,"has":i.has} for i in items])
+    if modulo == 'lotes': return jsonify([{"id":i.id,"nombre":i.nombre,"cultivo":i.cultivo,"has":i.has, "geometria":i.geometria} for i in items])
     if modulo == 'lluvias': return jsonify([{"id":i.id,"mm":i.mm,"fecha":i.fecha} for i in items])
     if modulo == 'gastos': return jsonify([{"id":i.id,"concepto":i.concepto,"monto":i.monto,"fecha":i.fecha} for i in items])
 
@@ -81,7 +81,7 @@ def acciones(modulo, id):
 @app.route('/reset')
 def reset():
     db.drop_all(); db.create_all()
-    return "SISTEMA AGROPECUARIO V14 REESTABLECIDO"
+    return "AGRONEXO V15 - SISTEMA TOTAL REESTABLECIDO"
 
 if __name__ == '__main__':
     with app.app_context(): db.create_all()
